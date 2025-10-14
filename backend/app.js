f470f29215a5cfa841d8c3e4a0745e57f1814403
ulcitrinitas <uli.citrinitas@gmail.com>
2025-10-13 22:23:51 -0300

    criada conexão com o banco de dados
    criada teste de querys
    
    movido os arquivos para src
    
    criada funções para conexão

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-import { connect_db} from "./db"
-
-// mysql://:@:/
-
-const db_info = { user: "root", pass: "mypass", host: "localhost", port: 3308, db: "lp_crud" };
-
-const conn = connect_db(db_info);
-
-console.log(conn)

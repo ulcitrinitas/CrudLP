7f74d9f526e09f00ee8cffa8241c5317de08edf0
ulcitrinitas <uli.citrinitas@gmail.com>
2025-10-13 21:32:24 -0300

    criada função para conectar com o banco de dados

---
+++ backend/db.ts
@@ -0,0 +1,7 @@
+import mysql from "mysql2/promise"
+
+async function connect(user: string, pass: string, host: string, port: Number, db: string) {
+    // const mysql_url = "mysql://root:mypass@localhost:3308/lp_crud";
+    const mysql_url = `mysql://${user}:${pass}@${host}:${port}/${db}`;
+    const conn = await mysql.createConnection(mysql_url);
+}

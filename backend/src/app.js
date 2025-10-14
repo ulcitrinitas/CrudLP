39a9540ad2bc333a91652aaf62ad9377906e33d6
ulcitrinitas <uli.citrinitas@gmail.com>
2025-10-13 22:02:29 -0300

    mudado para arquivos js

---
+++ backend/src/app.js
@@ -0,0 +1,9 @@
+import { connect_db, User } from "./db"
+
+// mysql://:@:/
+
+const db_info = { user: "root", pass: "mypass", host: "localhost", port: 3308, db: "lp_crud" };
+
+const conn = connect_db(db_info);
+
+console.log(conn)

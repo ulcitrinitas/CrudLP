5019e57d1b9c96ca303041a6929656521114c0f5
Ulisses Silva <uli.citrinitas@gmail.com>
2025-10-23 10:17:27 -0300

    alteradas tabelas para apenas fornecedor e bebidas

--- backend/db/tables.sql
+++ backend/db/tables.sql
@@ -2,22 +2,9 @@ CREATE DATABASE lp_crud;
 
 USE lp_crud;
 
-/* DROP TABLE Bebida;
-DROP TABLE Medida;
-DROP TABLE Marca;
-DROP TABLE Fornecedor;
-DROP TABLE Endereco;
- */
+create database crudLP;
 
-CREATE TABLE
-    Endereco (
-        codigo_end INT AUTO_INCREMENT PRIMARY KEY,
-        cep CHAR(9) NOT NULL,
-        logradouro VARCHAR(256),
-        cidade VARCHAR(64),
-        UF CHAR(2),
-        pais VARCHAR(128)
-    );
+use crudLP;
 
 CREATE TABLE
     Fornecedor (
@@ -26,23 +13,8 @@ CREATE TABLE
         cnpj CHAR(18) NOT NULL,
         email VARCHAR(128),
         pais_cod VARCHAR(5),
-        endereco_cod INT,
-        FOREIGN KEY (endereco_cod) REFERENCES Endereco (codigo_end)
-    );
-
-CREATE TABLE
-    Marca (
-        marca_cod INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(128),
-        forn_cod INT,
-        FOREIGN KEY (forn_cod) REFERENCES Fornecedor (id)
-    );
-
-CREATE TABLE
-    Medida (
-        med_cod INT AUTO_INCREMENT UNIQUE PRIMARY KEY,
-        sigla CHAR(3),
-        nome_med VARCHAR(64)
+        endereco VARCHAR(512),
+        uf CHAR(2)
     );
 
 CREATE TABLE
@@ -55,9 +27,7 @@ CREATE TABLE
         tipo VARCHAR(128),
         volume_med INT,
         forn_cod INT,
-        marca_cod INT,
-        FOREIGN KEY (marca_cod) REFERENCES Marca (marca_cod),
-        FOREIGN KEY (volume_med) REFERENCES Medida (med_cod),
+        marca VARCHAR(128),
         FOREIGN KEY (forn_cod) REFERENCES Fornecedor (id)
     );
 

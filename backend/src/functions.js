501e0ce989db602356338a530828d4387454ef2a
Ulisses Silva <uli.citrinitas@gmail.com>
2025-11-08 14:42:14 -0300

    criada função fornVetor

--- backend/src/functions.js
+++ backend/src/functions.js
@@ -27,3 +27,31 @@ export function bebidasVetor(req_body) {
 
     return v;
 }
+
+export function fornVetor(req_body) {
+
+    let v = [];
+
+    if (Array.isArray(req_body)) {
+        v = req_body.map((b) => {
+            return [
+                b.beb_nome,
+                b.qtde,
+                b.preco_uni,
+                b.volume,
+                b.tipo,
+                b.forn_cod,
+                b.marca
+            ];
+        });
+    }
+    else {
+        for (const chave in req_body) {
+            v.push(req_body[chave]);
+        }
+    }
+
+    console.log(v);
+
+    return v;
+}

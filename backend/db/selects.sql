433743c984c8f3ede1e7dc4856908d4d19821204
Ulisses Silva <uli.citrinitas@gmail.com>
2025-11-13 10:53:28 -0300

    movido tudo para tables

--- backend/db/selects.sql
+++
@@ -1,7 +0,0 @@
--- seleciona todos os campos
-select beb_nome, qtde, preco_uni, volume, tipo, volume_med, forn_cod, marca, nome, cnpj, email, pais_cod, endereco, uf
-from bebida b
-join fornecedor f on f.id = b.forn_cod;
-
-
-
